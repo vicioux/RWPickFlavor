0c62f28824bf6c6ce0e9c9e041a07588c5db8b97
Sergio Andres Orozco Builes <sergio.orozco@co-it004539.globant.com>
2018-11-08 09:42:04 -0500

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Sergio Andres Orozco Builes on 11/8/18.
+//  Copyright © 2018 Sergio Andres Orozco Builes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
